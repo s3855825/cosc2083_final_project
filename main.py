2722aff99853f26c08e47f55b2f8ec6546ff6a7a
Minh Duc Tran <minhduc.tran@mail.utoronto.ca>
2020-09-24 13:35:06 +0700

    we'll see

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from app.views import app
 from app import db
+import os
 
 if __name__ == "__main__":
     try:
         db.create_all()
-        app.run(host='localhost', threaded=True, debug=True)
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port, threaded=True, debug=True)
     except Exception as e:
         print(e)
