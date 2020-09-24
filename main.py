bd8f5f2605528e259f92893e297db85974997e39
Tran Minh Duc <62131944+s3855825@users.noreply.github.com>
2020-09-24 14:44:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     try:
         db.create_all()
         port = int(os.environ.get('PORT', 5000))
-        app.run(host='localhost', port=port, threaded=True, debug=True)
+        app.run(host='0.0.0.0', port=port, threaded=True, debug=True)
     except Exception as e:
         print(e)
